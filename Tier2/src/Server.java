c4752fb568ba46c76fbf13d484028e3e8c2f3f58
SanderKirchert <Sanders@post.cybercity.dk>
2021-09-22 16:50:29 +0200

    rename dbserver

--- Tier2/src/Server.java
+++ Tier2/src/Server.java
@@ -1,2 +1,41 @@
-public class Server {
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Server implements IServer{
+
+
+    private Map<String, ICallbackClient> connectedClients;
+
+
+    public void startServer() throws RemoteException, AlreadyBoundException {
+        Registry registry = LocateRegistry.createRegistry(1099);
+
+        registry.bind(NameConstants.DBServer.name(), this);
+
+        UnicastRemoteObject.exportObject(this, 1100);
+
+        System.out.println("Server Started!");
+
+        connectedClients = new HashMap<>();
+    }
+
+    @Override
+    public boolean createAccount(Account account) {
+        return false;
+    }
+
+    @Override
+    public boolean deposit(Account account, double amount) {
+        return false;
+    }
+
+    @Override
+    public boolean withdraw(Account account, double amount) {
+        return false;
+    }
 }
--- Tier3/src/DatabaseServer.java
+++ Tier3/src/DatabaseServer.java
@@ -1,8 +1,21 @@
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseServer implements IDbServer
 {
+
+
+    private DatabaseServer() throws SQLException {
+        DriverManager.registerDriver(new org.postgresql.Driver());
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/postgres?currentSchema=banking",
+                "username", "pasword");
+    }
+
 
     @Override
     public boolean createAccount(Account account) {
@@ -18,4 +31,8 @@ public class DatabaseServer implements IDbServer
     public boolean withdraw(Account account, double amount) {
         return false;
     }
+
+
+
+
 }
